99231a9d7c561e413df152a5a048c3711d5603d6
Mehmet Furkan Doğan <mehmetfurkandogan@hotmail.com>
2023-06-16 20:56:23 +0300

    Update single_pulse.ino

--- codes/single_pulse/single_pulse.ino
+++ codes/single_pulse/single_pulse.ino
@@ -1,13 +1,16 @@
 // Define the digital pin for output
-const int outputPin = 2;
+const int outputPin = 4;
+int step_pulse_time = 100;
+int step_idle_delay = 25;
+int number_of_pulses = 100;
 
 void setup() {
   // Initialize serial communication
   Serial.begin(9600);
-  
+
   // Set the output pin as an output
   pinMode(outputPin, OUTPUT);
-  
+
   // Set initial state to LOW
   digitalWrite(outputPin, LOW);
 }
@@ -15,12 +18,15 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     char receivedChar = Serial.read();
-    
+
     // Check the received character
     if (receivedChar == 'H' || receivedChar == 'h') {
-      digitalWrite(outputPin, HIGH);  // Set the output pin HIGH
-      delayMicroseconds(100);
-      digitalWrite(outputPin, LOW);   // Set the output pin LOW
-
+      for (int i = 0 ; i < number_of_pulses; i++) {
+        digitalWrite(outputPin, HIGH);  // Set the output pin HIGH
+        delayMicroseconds(step_pulse_time);
+        digitalWrite(outputPin, LOW);   // Set the output pin LOW
+        delay(step_idle_delay);
+      }
+    }
   }
 }
