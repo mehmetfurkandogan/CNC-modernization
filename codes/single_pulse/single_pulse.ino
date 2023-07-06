fd6a058fd2b54ce67872a702d38c860039ad5136
Mehmet Furkan Doğan <mehmetfurkandogan@hotmail.com>
2023-07-06 13:56:46 +0300

    Update single_pulse.ino
    Co-Authored-By: YalinSahin <102316233+YalinSahin@users.noreply.github.com>
    Co-Authored-By: egemenilikmen <122719643+egemenilikmen@users.noreply.github.com>

--- codes/single_pulse/single_pulse.ino
+++ codes/single_pulse/single_pulse.ino
@@ -1,6 +1,8 @@
 // Define the digital pin for output
-const int outputPin = 2;
-const int directionPin = 5;
+const int outputPin_x = 2;
+const int directionPin_x = 5;
+const int outputPin_z = 4;
+const int directionPin_z = 7;
 int step_pulse_time = 1000;
 int pulse_delay = 100;
 int number_of_pulses = 20;
@@ -9,13 +11,17 @@ void setup() {
   // Initialize serial communication
   Serial.begin(9600);
 
-  // Set the output pin as an output
-  pinMode(outputPin, OUTPUT);
-  pinMode(directionPin, OUTPUT);
+  // Set the output pins as output
+  pinMode(outputPin_x, OUTPUT);
+  pinMode(directionPin_x, OUTPUT);
+  pinMode(outputPin_z, OUTPUT);
+  pinMode(directionPin_z, OUTPUT);
 
   // Set initial state to LOW
-  digitalWrite(outputPin, LOW);
-  digitalWrite(directionPin, HIGH);
+  digitalWrite(outputPin_x, LOW);
+  digitalWrite(directionPin_x, HIGH);
+  digitalWrite(outputPin_z, LOW);
+  digitalWrite(directionPin_z, HIGH);
 }
 
 void loop() {
@@ -23,15 +29,28 @@ void loop() {
     char receivedChar = Serial.read();
 
     // Check the received character
-    if (receivedChar == 'H' || receivedChar == 'h') {
-      digitalWrite(directionPin, HIGH);
+    if (receivedChar == 'X' || receivedChar == 'x') {
+      digitalWrite(directionPin_x, HIGH);
       for (int i = 0 ; i < number_of_pulses; i++) {
-        digitalWrite(outputPin, HIGH);  // Set the output pin HIGH
+        digitalWrite(outputPin_x, HIGH);  // Set the output pin HIGH
         delayMicroseconds(step_pulse_time);
-        digitalWrite(outputPin, LOW);   // Set the output pin LOW
+        digitalWrite(outputPin_x, LOW);   // Set the output pin LOW
         delay(pulse_delay);
       }
       Serial.println("ok.");
     }
+
+    else if (receivedChar == 'Z' || receivedChar == 'z') {
+      digitalWrite(directionPin_z, HIGH);
+      for (int i = 0 ; i < number_of_pulses; i++) {
+        digitalWrite(outputPin_z, HIGH);  // Set the output pin HIGH
+        delayMicroseconds(step_pulse_time);
+        digitalWrite(outputPin_z, LOW);   // Set the output pin LOW
+        delay(pulse_delay);
+      }
+      Serial.println("ok.");
+    }
+
+    
   }
 }
